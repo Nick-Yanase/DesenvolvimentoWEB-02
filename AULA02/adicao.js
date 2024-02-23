b9afc159dffd3e0a9b6f442a22fc181f763abb47
Nicolas Yanase <142536049+Nick-Yanase@users.noreply.github.com>
2024-02-23 10:29:40 -0300

    Update node.js

---
+++ AULA02/adicao.js
@@ -0,0 +1,6 @@
+var adicao = function(n1, n2){
+    return n1 + n2
+}
+
+module.exports = adicao //para usar em outro arquivos
+
---
+++ AULA02/app.js
@@ -0,0 +1,69 @@
+var adicao = require('./adicao')
+var subtracao = require('./subtracao')
+var div = require('./divisao')
+var multi = require('./multiplicacao') //nome arquivo
+
+console.log("Multiplicaçâo: "+multi(10,2)) //chamei a funcao subtracao e passei o parametro pra ela efetuar sseus calculos
+console.log("adição: "+adicao(10,2))
+console.log("Subtração: "+subtracao(10,2))
+console.log("Divisão: "+div(10,2))
+
+/*console.log("Mensagem de texto!")
+
+
+
+var n1 =5
+var n2 =5
+
+var adicao = n1 + n2
+var subtracao = n1 - n2
+var mult = n1 * n2
+var div = n1 / n2
+
+console.log ("Adição: " +adicao)
+console.log ("Subtração: " +subtracao)
+console.log ("Multiplicação: " +mult)
+console.log ("Dvisão: " +div)
+*/
+
+/*var n1 =10
+
+if (n1 <= 10){
+    console.log("Certo, menor ou iugal que 10")
+}else{
+    console.log("Errado, maior que 10")
+}
+*/
+
+
+/*
+for(var n1 = 0;n1<=10;n1++){
+    console.log("N1: " + n1)
+}
+
+
+var n1 =0
+do {
+    console.log("N1: "+n1)
+    n1++
+}while(n1 <=10)*/
+
+
+
+/* GITHUBBBBBBB
+instalar git, pra usar dentro do cmd
+
+git init
+
+dir pra ver arquivo
+
+git add *
+
+git commit -m "Primeiro arquivo de node"
+
+git branch -m main
+
+git push -u origin main
+
+dps abre um popup pra logar por meio do navegador 
+*/
