68e042586501984e14479cdb660487306bf9653f
Nicolas Yanase <142536049+Nick-Yanase@users.noreply.github.com>
2024-03-26 10:44:21 -0300

    isso ai

---
+++ AULA06/app.js
@@ -0,0 +1,46 @@
+const express = require("express")
+const app = express();
+const handlebars = require('express-handlebars').engine
+const bodyParser = require("body-parser");
+const post = require('./models/post');
+
+app.engine("handlebars", handlebars({ defaultLayout: "main" }))
+app.set('view engine', 'handlebars')
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => { // get mostra od dados
+    res.render('index')
+})
+
+app.post("/cadastrar", (req, res) => { //post pega dados e joga dentro do banco
+    post.create({
+        nome: req.body.nome,
+        telefone: req.body.telefone,
+        origem: req.body.origem,
+        data_contato: req.body.data_contato,
+        observacao: req.body.observacao
+    }).then(() => {
+        res.redirect('/');
+    }).catch((erro) => {
+        console.log('Falha ao cadastrar os dados ' + erro)
+    })
+
+})
+
+app.get("/consultar", (req, res) => {
+    res.render('consultar')
+})
+
+
+
+app.get("/atualizar", (req, res) => {
+    res.render('atualizar')
+})
+
+
+
+app.listen(8082, () => {
+    console.log('server rodando')
+})
